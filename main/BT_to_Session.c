39cd3a40f6fbee3f89327c614ed198aa369aaed4
Lemon_zz <pavlov.d60@gmail.com>
2022-07-06 19:56:17 +0300

    commit

--- main/BT_to_Session.c
+++ main/BT_to_Session.c
@@ -77,7 +77,6 @@ void parse_bt_packet(struct bt_packet *rx_packet, uint32_t handle)
         incoming_session.act_time       = &rx_packet->payload[1];
         incoming_session.idle_time      = &rx_packet->payload[2];
         incoming_session.pwm            = &rx_packet->payload[3];
-        *incoming_session.pwm           = (*incoming_session.pwm) * 12;
         
         block_exec(&incoming_session);
         send_session_ack(&incoming_session.position[0], handle);
@@ -125,9 +124,9 @@ void parse_bt_packet(struct bt_packet *rx_packet, uint32_t handle)
             next_block.act_time     = &incoming_session.act_time[k];
             next_block.idle_time    = &incoming_session.idle_time[k];
 
-            uint8_t pwm = get_data_from_nvs_by_position(next_block.position[0]);
+            uint8_t pwm = get_pwm_from_nvs_by_position(next_block.position[0]);
             next_block.pwm = &pwm;
-
+            
             send_session_ack(&next_block.position[0], handle);
             block_exec(&next_block);
         }
@@ -136,16 +135,19 @@ void parse_bt_packet(struct bt_packet *rx_packet, uint32_t handle)
 
     case calibration:
         set_data_to_nvs_by_position(rx_packet->payload[0], rx_packet->payload[1]);
+        send_batt_data(handle);
         send_ACK(handle);
         break;
 
     case pause:
         stop_pwm();
+        send_batt_data(handle);
         send_ACK(handle);
         break;
 
     case stop:
         stop_pwm();
+        send_batt_data(handle);
         send_ACK(handle);
         break;
 
@@ -154,16 +156,34 @@ void parse_bt_packet(struct bt_packet *rx_packet, uint32_t handle)
         uint8_t *new_tactors;
         new_tactors = calloc(5, sizeof(uint8_t));
 
-        for (size_t i = 0; i < 5; i++)
+        for (uint8_t i = 0; i < 5; i++)
         {
            new_tactors[i] = rx_packet->payload[i];
         }
 
         set_data_to_nvs_tactors(new_tactors);
+        send_batt_data(handle);
+        send_ACK(handle);
+        break;
+
+    case CMD_MOVEMENT_TIME:
+        ; 
+        uint8_t *new_retract, *new_forward;
+        new_retract = calloc(5, sizeof(uint8_t));//for future 
+        new_forward = calloc(5, sizeof(uint8_t));
+
+        for(uint8_t i = 0; i<5; i++){
+            new_forward[i] = (rx_packet->payload[0]+1);
+            new_retract[i] = (rx_packet->payload[1]+1);
+        }
+
+        set_data_to_motor_move(new_retract, new_forward);
+        send_batt_data(handle);
         send_ACK(handle);
         break;
 
     case ACK:
+        send_batt_data(handle);
         send_ACK(handle);
         break;
 
@@ -171,6 +191,7 @@ void parse_bt_packet(struct bt_packet *rx_packet, uint32_t handle)
         break;
 
     case state_info:
+        send_batt_data(handle);
         send_ACK(handle);
         break;
 
@@ -191,6 +212,25 @@ void send_session_ack(uint8_t *position, uint32_t handle)
     buf[1] = tx_packet.length;
     buf[2] = tx_packet.payload[0];
     tx_packet.crc16 = crc16Calc(buf, 2);
+    send_to_bt(&tx_packet, handle);
+}
+
+
+void send_batt_data(uint32_t handle)
+{
+    struct bt_packet tx_packet;
+    tx_packet.ID = 0x12;
+    tx_packet.length = 0x06;
+    tx_packet.payload = calloc(sizeof(uint8_t), sizeof(uint8_t));
+    uint32_t batt_voltage = ((get_ADC_data(ADC1, batt_chan)*31) / 11) / 10; //divider 20k/11k so adc = voltage*11000/(20000+11000) so 31*adc=11*voltage. Divide by 10 cause NRF variation
+    printf("BUTT: %d\n", batt_voltage);
+    memcpy(tx_packet.payload, &batt_voltage, sizeof(uint16_t));
+    uint8_t buf[4];
+    buf[0] = tx_packet.ID;
+    buf[1] = tx_packet.length;
+    buf[2] = tx_packet.payload[0];
+    buf[3] = tx_packet.payload[1];
+    tx_packet.crc16 = crc16Calc(buf, 4);
     send_to_bt(&tx_packet, handle);
 }
 
@@ -220,18 +260,7 @@ void send_to_bt(struct bt_packet *tx_packet, uint32_t handle)
     }
 
     if (tx_packet->length == 5)
-    { // to do:
-
-        // ESP_LOG_BUFFER_CHAR(BT_TAG,"Send:",6);
-        // ESP_LOG_BUFFER_CHAR(BT_TAG,"Packet ID:",12);
-        // esp_log_buffer_hex("", &tx_packet->ID, 1);
-        // ESP_LOG_BUFFER_CHAR(BT_TAG,"Packet length:",14);
-        // esp_log_buffer_hex("", &tx_packet->length, 1);
-        // ESP_LOG_BUFFER_CHAR(BT_TAG, "Packet payload:", 14);
-        // esp_log_buffer_hex("", tx_packet->payload, 1);
-        // ESP_LOG_BUFFER_CHAR(BT_TAG,"Packet crc:",12);
-        // esp_log_buffer_hex("", &tx_packet->crc16, 2);
-
+    { 
         uint8_t packet[5];
         packet[0] = tx_packet->ID;
         packet[1] = tx_packet->length;
@@ -240,11 +269,20 @@ void send_to_bt(struct bt_packet *tx_packet, uint32_t handle)
         packet[4] = tx_packet->crc16 >> 8;
         ESP_ERROR_CHECK(esp_spp_write(handle, tx_packet->length, packet));
     }
+    if (tx_packet->length == 6){
+        uint8_t packet[6];
+        packet[0] = tx_packet->ID;
+        packet[1] = tx_packet->length;
+        packet[2] = tx_packet->payload[0];
+        packet[3] = tx_packet->payload[1];
+        packet[4] = tx_packet->crc16;
+        packet[5] = tx_packet->crc16 >> 8;
+        ESP_ERROR_CHECK(esp_spp_write(handle, tx_packet->length, packet));
+    }
 }
 
 void num2permutation(uint8_t CYCLES_PER_BLOCK, uint8_t data, uint8_t *return_Arr)
 {
-
     uint8_t n = CYCLES_PER_BLOCK;
     uint8_t k = data - 1;
     bool *was;
@@ -281,6 +319,18 @@ uint8_t fact(uint8_t num)
         return num * fact(num - 1);
 }
 
+//map pwm
+uint8_t pwm_out_min = 127;
+uint8_t pwm_out_max = 0;
+uint8_t pwm_in_min = 0;
+uint8_t pwm_in_max = 10;
+long pwm_map(long x, long in_min, long in_max)
+{
+  return (x - in_min) * (pwm_out_max - pwm_out_min) / (in_max - in_min) + pwm_out_min;
+}
+
+
+
 void block_exec(struct session *incoming_session)
 {
 
@@ -297,25 +347,34 @@ void block_exec(struct session *incoming_session)
     esp_log_buffer_hex("", incoming_session->position, 1);
     
     
-    uint8_t *tactors_array;
+    uint8_t *tactors_array, *retract_time, *forward_time;
     tactors_array = calloc(5, sizeof(uint8_t));
+    retract_time = calloc(5, sizeof(uint8_t));
+    forward_time = calloc(5, sizeof(uint8_t));
 
     get_data_from_nvs_tactors(tactors_array);
+    get_data_from_motor_move(retract_time, forward_time);
 
     ESP_LOG_BUFFER_CHAR(BT_TAG, "Saved Position", sizeof("Saved Position"));
     esp_log_buffer_hex("", &tactors_array[(int)incoming_session->position[0]-1], 1);
-
+    uint8_t pwm1 = pwm_map(incoming_session->pwm[0], pwm_in_min, pwm_in_max);
+    printf("PWM 1: %d\n", pwm1);
     switch (tactors_array[(int)incoming_session->position[0]-1])
     {
     case 01:
         vTaskDelay(((incoming_session->delay_time[0]) * 1000) / portTICK_RATE_MS);
         set_motors_en(DRIVERS_ON);
-        set_PWM(MOTOR_1_IN_1, 127 - incoming_session->pwm[0]);                   //push   
-        set_PWM(MOTOR_1_IN_2, 127 + incoming_session->pwm[0]);                   // drv8601 is differential
+        set_PWM(MOTOR_1_IN_1, pwm1);                   //push   
+        set_PWM(MOTOR_1_IN_2, 255-pwm1);                   // drv8601 is differential
+        vTaskDelay(forward_time[0]*250/ portTICK_RATE_MS);
+        set_motors_en(DRIVERS_OFF);                   
         vTaskDelay(((incoming_session->act_time[0]) * 1000) / portTICK_RATE_MS); // wait
-        set_PWM(MOTOR_1_IN_1, 200);                                              // retract
-        set_PWM(MOTOR_1_IN_2, 0);
-        vTaskDelay(100 / portTICK_RATE_MS);
+
+        set_motors_en(DRIVERS_ON);
+        set_PWM(MOTOR_1_IN_1, 255-pwm1);                                              // retract
+        set_PWM(MOTOR_1_IN_2, pwm1);
+        vTaskDelay(retract_time[0]*250 / portTICK_RATE_MS);
+
         set_PWM(MOTOR_1_IN_1, LEDC_DUTY_IDLE); // set idle
         set_PWM(MOTOR_1_IN_2, LEDC_DUTY_IDLE);
         set_motors_en(DRIVERS_OFF);//cool off
@@ -325,12 +384,17 @@ void block_exec(struct session *incoming_session)
     case 02:
         vTaskDelay(((incoming_session->delay_time[0]) * 1000) / portTICK_RATE_MS);
         set_motors_en(DRIVERS_ON);
-        set_PWM(MOTOR_2_IN_1, 127 - incoming_session->pwm[0]);                   //push   
-        set_PWM(MOTOR_2_IN_2, 127 + incoming_session->pwm[0]);                        
-        vTaskDelay(((incoming_session->act_time[0]) * 1000) / portTICK_RATE_MS); // wait
-        set_PWM(MOTOR_2_IN_1, 200);                                              // retract
-        set_PWM(MOTOR_2_IN_2, 0);
-        vTaskDelay(100 / portTICK_RATE_MS);
+        set_PWM(MOTOR_2_IN_1, pwm1);                   //push   
+        set_PWM(MOTOR_2_IN_2, 255 - pwm1);
+        vTaskDelay(forward_time[1]*250/ portTICK_RATE_MS);
+        set_motors_en(DRIVERS_OFF);                        
+        vTaskDelay(((incoming_session->act_time[0]) * 1000) / portTICK_RATE_MS);
+         // wait
+        set_motors_en(DRIVERS_ON);
+        set_PWM(MOTOR_2_IN_1, 255 - pwm1);                                              // retract
+        set_PWM(MOTOR_2_IN_2, pwm1);
+        vTaskDelay(retract_time[1]*250 / portTICK_RATE_MS);
+
         set_PWM(MOTOR_2_IN_1, LEDC_DUTY_IDLE); // set idle
         set_PWM(MOTOR_2_IN_2, LEDC_DUTY_IDLE);
         set_motors_en(DRIVERS_OFF);
@@ -339,12 +403,17 @@ void block_exec(struct session *incoming_session)
     case 03:
         vTaskDelay(((incoming_session->delay_time[0]) * 1000) / portTICK_RATE_MS);
         set_motors_en(DRIVERS_ON);
-        set_PWM(MOTOR_3_IN_1, 127 - incoming_session->pwm[0]);                   //push   
-        set_PWM(MOTOR_3_IN_2, 127 + incoming_session->pwm[0]);                       
+        set_PWM(MOTOR_3_IN_1, pwm1);                   //push   
+        set_PWM(MOTOR_3_IN_2, 255 - pwm1);
+        vTaskDelay(forward_time[2]*250/ portTICK_RATE_MS);
+        set_motors_en(DRIVERS_OFF);                       
         vTaskDelay(((incoming_session->act_time[0]) * 1000) / portTICK_RATE_MS); // wait
-        set_PWM(MOTOR_3_IN_1, 200);                                              // retract
-        set_PWM(MOTOR_3_IN_2, 0);
-        vTaskDelay(100 / portTICK_RATE_MS);
+        
+        set_motors_en(DRIVERS_ON);
+        set_PWM(MOTOR_3_IN_1, 255 - pwm1);                                              // retract
+        set_PWM(MOTOR_3_IN_2, pwm1);
+        vTaskDelay(retract_time[2]*250 / portTICK_RATE_MS);
+
         set_PWM(MOTOR_3_IN_1, LEDC_DUTY_IDLE); // set idle
         set_PWM(MOTOR_3_IN_2, LEDC_DUTY_IDLE);
         set_motors_en(DRIVERS_OFF);
@@ -353,12 +422,17 @@ void block_exec(struct session *incoming_session)
     case 04:
         vTaskDelay(((incoming_session->delay_time[0]) * 1000) / portTICK_RATE_MS);
         set_motors_en(DRIVERS_ON);
-        set_PWM(MOTOR_4_IN_1, 127 - incoming_session->pwm[0]);                   //push   
-        set_PWM(MOTOR_4_IN_2, 127 + incoming_session->pwm[0]);                        
+        set_PWM(MOTOR_4_IN_1, pwm1);                   //push   
+        set_PWM(MOTOR_4_IN_2, 255 - pwm1);
+        vTaskDelay(forward_time[3]*250/ portTICK_RATE_MS);
+        set_motors_en(DRIVERS_OFF);                        
         vTaskDelay(((incoming_session->act_time[0]) * 1000) / portTICK_RATE_MS); // wait
-        set_PWM(MOTOR_4_IN_1, 200);                                              // retract
-        set_PWM(MOTOR_4_IN_2, 0);
-        vTaskDelay(100 / portTICK_RATE_MS);
+        
+        set_motors_en(DRIVERS_ON);
+        set_PWM(MOTOR_4_IN_1, 255-pwm1);                                              // retract
+        set_PWM(MOTOR_4_IN_2, pwm1);
+        vTaskDelay(retract_time[3]*250 / portTICK_RATE_MS);
+        
         set_PWM(MOTOR_4_IN_1, LEDC_DUTY_IDLE); // set idle
         set_PWM(MOTOR_4_IN_2, LEDC_DUTY_IDLE);
         set_motors_en(DRIVERS_OFF);
@@ -367,12 +441,17 @@ void block_exec(struct session *incoming_session)
     case 05:
         vTaskDelay(((incoming_session->delay_time[0]) * 1000) / portTICK_RATE_MS);
         set_motors_en(DRIVERS_ON);
-        set_PWM(MOTOR_5_IN_1, 127 - incoming_session->pwm[0]);                   //push   
-        set_PWM(MOTOR_5_IN_2, 127 + incoming_session->pwm[0]);                       
+        set_PWM(MOTOR_5_IN_1, pwm1);                   //push   
+        set_PWM(MOTOR_5_IN_2, 255 - pwm1);
+        vTaskDelay(forward_time[4]*250/ portTICK_RATE_MS);
+        set_motors_en(DRIVERS_OFF);                       
+        
         vTaskDelay(((incoming_session->act_time[0]) * 1000) / portTICK_RATE_MS); // wait
-        set_PWM(MOTOR_5_IN_1, 200);                                              // retract
-        set_PWM(MOTOR_5_IN_2, 0);
-        vTaskDelay(100 / portTICK_RATE_MS);
+        set_motors_en(DRIVERS_ON);
+        set_PWM(MOTOR_5_IN_1, 255 - pwm1);                                              // retract
+        set_PWM(MOTOR_5_IN_2, pwm1);
+        vTaskDelay(retract_time[4]*250 / portTICK_RATE_MS);
+        
         set_PWM(MOTOR_5_IN_1, LEDC_DUTY_IDLE); // set idle
         set_PWM(MOTOR_5_IN_2, LEDC_DUTY_IDLE);
         set_motors_en(DRIVERS_OFF);
@@ -387,7 +466,7 @@ void block_exec(struct session *incoming_session)
     set_motors_en(DRIVERS_OFF);
 }
 
-uint8_t get_data_from_nvs_by_position(uint8_t position)
+uint8_t get_pwm_from_nvs_by_position(uint8_t position)
 {
     nvs_handle_t nvs_handle;
     esp_err_t err;
@@ -480,13 +559,6 @@ void set_data_to_nvs_by_position(uint8_t position, uint8_t pwm)
     //incoming from 0 to 10
     nvs_handle_t nvs_handle;
     uint8_t pwm_to_save = pwm;
-
-    if(pwm_to_save == 0){
-        pwm_to_save = 25;
-    }
-
-    else 
-    pwm_to_save = pwm_to_save*25;
     ESP_ERROR_CHECK(nvs_open("storage", NVS_READWRITE, &nvs_handle));
     switch (position)
     {
@@ -529,6 +601,7 @@ void set_data_to_nvs_tactors(uint8_t *new_tactors){
 
 
 void get_data_from_nvs_tactors(uint8_t *tactors_array){
+
     nvs_handle_t nvs_handle;
     size_t *length;
     length = calloc(1, sizeof(size_t));
@@ -540,5 +613,39 @@ void get_data_from_nvs_tactors(uint8_t *tactors_array){
     tactors_array[4] = 5;
     ESP_ERROR_CHECK(nvs_open("storage", NVS_READWRITE, &nvs_handle));
     ESP_ERROR_CHECK(nvs_get_blob(nvs_handle,"Tactor data", tactors_array, length));
+    nvs_close(nvs_handle);
+}
+
+void set_data_to_motor_move(uint8_t *new_retract, uint8_t *new_forward){
+    nvs_handle_t nvs_handle;
+    size_t req_size = 5;
+    ESP_ERROR_CHECK(nvs_open("storage", NVS_READWRITE, &nvs_handle));
+    nvs_set_blob(nvs_handle,"Retract data", new_retract, req_size);
+    nvs_set_blob(nvs_handle,"Forward data", new_forward, req_size);
+    ESP_ERROR_CHECK(nvs_commit(nvs_handle));
+    nvs_close(nvs_handle);
+}
+
+void get_data_from_motor_move(uint8_t *saved_retract, uint8_t *saved_forward){
+    nvs_handle_t nvs_handle;
+    esp_err_t err;
+    size_t *length;
+    length = calloc(1, sizeof(size_t));
+    *length = 5;
+    saved_retract[0] = 4;
+    saved_retract[1] = 4;
+    saved_retract[2] = 4;
+    saved_retract[3] = 4;
+    saved_retract[4] = 4; //default 4*250 ms
+
+    saved_forward[0] = 4;
+    saved_forward[1] = 4;
+    saved_forward[2] = 4;
+    saved_forward[3] = 4;
+    saved_forward[4] = 4;
+
+    ESP_ERROR_CHECK(nvs_open("storage", NVS_READWRITE, &nvs_handle));
+    ESP_ERROR_CHECK(nvs_get_blob(nvs_handle,"Retract data", saved_retract, length));
+    ESP_ERROR_CHECK(nvs_get_blob(nvs_handle,"Forward data", saved_forward, length));
     nvs_close(nvs_handle);
 }
